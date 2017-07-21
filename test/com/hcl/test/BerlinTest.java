9ca7db16673d7c0c9115c8f88e9d4cd8b621f6a1
P Vivekanandan <vivekanandan_p@HCL.COM>
2017-07-21 08:27:21 +0530

    Berlinclock Assignment

---
+++ test/com/hcl/test/BerlinTest.java
@@ -0,0 +1,13 @@
+package com.hcl.test;
+
+import java.util.Calendar;
+
+public class BerlinTest {
+	
+	public static void main(String s[])
+	{
+		Calendar calInst =Calendar.getInstance();
+	 System.out.println(calInst.get(Calendar.SECOND));
+	}
+
+}
